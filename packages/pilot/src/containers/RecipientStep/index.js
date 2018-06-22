83ec42add46f653cf4d4fbcb0584588edbb78685
gmbarroso <barroso.guilherme@gmail.com>
2018-06-22 14:42:51 +0000

    recipients: trying to resolve some css issues

--- packages/pilot/src/containers/RecipientStep/index.js
+++ packages/pilot/src/containers/RecipientStep/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-// import PropTypes from 'prop-types'
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types'
 import {
   Button,
   Card,
@@ -7,6 +7,8 @@ import {
   CardContent,
   CardActions,
   RadioGroup,
+  FormInput,
+  FormCheckbox,
 } from 'former-kit'
 
 const options = [
@@ -15,7 +17,7 @@ const options = [
     value: 'physic',
   },
   {
-    name: 'PEssoa Jurídica',
+    name: 'Pessoa Jurídica',
     value: 'legal',
   },
 ]
@@ -37,11 +39,12 @@ class RecipientStep extends Component {
   }
 
   render () {
-    // const {
-    //   disabled,
-    //   error,
-    //   name,
-    // } = this.props
+    const {
+      disabled,
+      error,
+      name,
+      // size,
+    } = this.props
 
     const {
       value,
@@ -52,20 +55,32 @@ class RecipientStep extends Component {
         <Card>
           <CardTitle
             title="Identificação"
+            subtitle="Escolha qual tipo de pessoa do seu recebedor e preencha o documento"
           />
           <CardContent>
-            <p>Lorem ipsum dolor sit amet, consectetur</p>
-            <div>
+            <Fragment>
               <RadioGroup
                 options={options}
-                // name={name}
+                name={name}
                 onChange={this.handleChange}
                 value={value}
-                // disabled={disabled}
-                // error={error}
+                disabled={disabled}
+                error={error}
               />
-              <p>Selected: {value}</p>
-            </div>
+              {/* <p>Teste: {value}</p> */}
+              <FormInput
+                maxLength={12}
+                inputStyle="form"
+                type="text"
+                label="CNPJ"
+              />
+              <br />
+              <br />
+              <FormCheckbox
+                label="Quero incluir informações sobre a empresa e os sócios"
+                name="info"
+              />
+            </Fragment>
           </CardContent>
           <CardActions>
             <Button fill="outline">Cancelar</Button>
@@ -75,6 +90,20 @@ class RecipientStep extends Component {
       </div>
     )
   }
+}
+
+RecipientStep.propTypes = {
+  name: PropTypes.string,
+  disabled: PropTypes.bool,
+  error: PropTypes.string,
+  // size: PropTypes.number,
+}
+
+RecipientStep.defaultProps = {
+  name: '',
+  disabled: false,
+  error: '',
+  // size: 30,
 }
 
 export default RecipientStep
