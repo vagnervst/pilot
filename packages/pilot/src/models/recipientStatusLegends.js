ada22da582298e8d33f9140fefe7a36329b1a0f5
Ivan Micai da Silva <ivanmicai@gmail.com>
2018-06-28 16:23:54 -0300

    recipients-list: add Recipient Status Legend

---
+++ packages/pilot/src/models/recipientStatusLegends.js
@@ -0,0 +1,44 @@
+const status = {
+  paid: {
+    color: '#17c9b2',
+    text: 'Paga',
+    acronym: 'P',
+  },
+  registration: {
+    color: '#0b6e69',
+    acronym: 'CR',
+    text: 'Em Afiliacao',
+  },
+  affiliation: {
+    color: '#12517c',
+    acronym: 'AF',
+    text: 'Em Credenciamento',
+  },
+  active: {
+    color: '#17c9b2',
+    acronym: 'A',
+    text: 'Ativo',
+  },
+  refused: {
+    color: '#da272c',
+    acronym: 'R',
+    text: 'Recusado',
+  },
+  suspended: {
+    color: '#f16f27',
+    acronym: 'TS',
+    text: 'Temporariamente Suspenso',
+  },
+  blocked: {
+    color: '#fbbb27',
+    acronym: 'TB',
+    text: 'Temporariamente Bloqueado',
+  },
+  inactive: {
+    color: '#613fc7',
+    acronym: 'PD',
+    text: 'Permanetemente Desativo',
+  },
+}
+
+export default status
