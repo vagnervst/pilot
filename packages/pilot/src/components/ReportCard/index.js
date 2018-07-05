718b8d76eea0db69f29479161bb6ee29c752cab4
Amanda Albuquerque <amandlei@hotmail.com>
2018-07-05 15:33:12 +0000

    report-card: refactor component props

--- packages/pilot/src/components/ReportCard/index.js
+++ packages/pilot/src/components/ReportCard/index.js
@@ -1,145 +1,60 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import {
-  Button,
   CardContent,
   CardSection,
   CardSectionDoubleLineTitle,
   Legend,
-  Popover,
-  PopoverMenu,
 } from 'former-kit'
 import moment from 'moment'
-import DownloadIcon from 'emblematic-icons/svg/Download32.svg'
-import TrashIcon from 'emblematic-icons/svg/Trash32.svg'
-import ReprocessIcon from 'emblematic-icons/svg/Reprocess32.svg'
 import reportStatusLegend from '../../models/reportStatusLegend'
-
-const items = [
-  {
-    title: 'PDF',
-  },
-  {
-    title: 'Excel',
-  },
-  {
-    title: 'csv',
-  },
-]
-
-const renderActions = (report) => {
-  if (report.status === 'canceled') {
-    return (
-      <Fragment>
-        <Button
-          fill="outline"
-          icon={<TrashIcon width={16} height={16} />}
-          key="Item 1"
-          onClick={() => null}
-          size="default"
-        />
-
-        <Button
-          fill="outline"
-          icon={<ReprocessIcon width={16} height={16} />}
-          key="Item 1"
-          onClick={() => null}
-          size="default"
-        />
-      </Fragment>
-    )
-  }
-
-  return (
-    <Fragment>
-      <Button
-        fill="outline"
-        icon={<TrashIcon width={16} height={16} />}
-        key="Item 1"
-        onClick={() => null}
-        size="default"
-      />
-      <Fragment>
-        <Popover
-          content={
-            <Fragment>
-              <div>
-                <strong>Teste</strong>
-              </div>
-              <PopoverMenu items={items} />
-            </Fragment>
-          }
-        >
-          <Button
-            fill="outline"
-            icon={<DownloadIcon width={16} height={16} />}
-            onClick={() => null}
-            size="default"
-          />
-        </Popover>
-      </Fragment>
-    </Fragment>
-  )
-}
 
 class ReportCard extends React.Component {
   constructor () {
     super()
-    this.handleClickItem = this.handleClickItem.bind(this)
-  }
-
-  handleClickItem () {
-    const {
-      report,
-      onClick,
-    } = this.props
-
-    onClick(report.id)
+    this.state = {
+      cardExpanded: false,
+    }
   }
 
   render () {
     const {
-      cardExpanded,
       report,
       subTitle,
       title,
+      actions,
     } = this.props
     return (
-      <CardContent>
-        <CardSection>
-          <CardSectionDoubleLineTitle
-            actions={renderActions(report)}
-            collapsed={!cardExpanded}
-            icon={
-              <Legend
-                color={reportStatusLegend[report.status].color}
-                acronym={reportStatusLegend[report.status].acronym}
-                hideLabel
-              />
-            }
-            onClick={() => null}
-            subTitle={subTitle}
-            title={title}
-          />
-          {cardExpanded &&
-            <div>
-              <CardContent>
-                <CardSection>
-                  Filtros
-                  <p>Status: {report.status}</p>
-                </CardSection>
-              </CardContent>
-            </div>
-            }
-        </CardSection>
-      </CardContent>
+      <CardSection>
+        <CardSectionDoubleLineTitle
+          actions={actions}
+          collapsed={!this.state.cardExpanded}
+          icon={
+            <Legend
+              color={reportStatusLegend[report.status].color}
+              acronym={reportStatusLegend[report.status].acronym}
+              hideLabel
+            />
+          }
+          onClick={
+            () => this.setState({ cardExpanded: !this.state.cardExpanded })
+          }
+          subtitle={subTitle}
+          title={title}
+        />
+        {this.state.cardExpanded &&
+          <CardContent>
+            Filtros
+            <p>Status: {report.status}</p>
+          </CardContent>
+        }
+      </CardSection>
     )
   }
 }
 
 ReportCard.propTypes = {
-  cardExpanded: PropTypes.bool.isRequired,
-  onClick: PropTypes.func.isRequired,
+  actions: PropTypes.func.isRequired,
   report: PropTypes.shape({
     data: PropTypes.shape({
       company_id: PropTypes.string,
--- packages/pilot/stories/components/ReportCard/index.js
+++ packages/pilot/stories/components/ReportCard/index.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import {
+  Button,
+  Card,
+  Popover,
+  PopoverMenu,
+} from 'former-kit'
 import ReportCard from '../../../src/components/ReportCard'
 import moment from 'moment'
+import DownloadIcon from 'emblematic-icons/svg/Download32.svg'
+import TrashIcon from 'emblematic-icons/svg/Trash32.svg'
+import ReprocessIcon from 'emblematic-icons/svg/Reprocess32.svg'
+import { action } from '@storybook/addon-actions'
+
+const items = [
+  {
+    title: 'PDF',
+    action: action('pdf'),
+  },
+  {
+    title: 'Excel',
+    action: action('excel'),
+  },
+  {
+    title: 'csv',
+    action: action('csv'),
+  },
+]
 
 const report = {
   object: 'report',
@@ -15,6 +40,61 @@ const report = {
   url: 'http://s3.amazonaws.com/tract-live/public/plans_cjh9eew06000012rxte25jpkr.json?AWSAccessKeyId=AKIAIUD2DGLYBPF7X3FA&Expires=1526493233&Signature=ce4THQeQV%2Ft%2BDPUnbTHSYo51mCI%3D',
   created_at: '2018-05-16T17:47:02.035Z',
   updated_at: '2018-05-16T17:47:02.596Z',
+}
+
+const renderActions = (report) => {
+  if (report.status === 'canceled') {
+    return (
+      <Fragment>
+        <Button
+          fill="outline"
+          icon={<TrashIcon width={16} height={16} />}
+          key="Item 1"
+          onClick={action('delete')}
+          size="default"
+        />
+
+        <Button
+          fill="outline"
+          icon={<ReprocessIcon width={16} height={16} />}
+          key="Item 1"
+          onClick={action('reprocess')}
+          size="default"
+        />
+      </Fragment>
+    )
+  }
+
+  return (
+    <Fragment>
+      <Button
+        fill="outline"
+        icon={<TrashIcon width={16} height={16} />}
+        key="Item 1"
+        onClick={action('delete')}
+        size="default"
+      />
+      <Fragment>
+        <Popover
+          content={
+            <Fragment>
+              <div>
+                <strong>Teste</strong>
+              </div>
+              <PopoverMenu items={items} />
+            </Fragment>
+          }
+        >
+          <Button
+            fill="outline"
+            icon={<DownloadIcon width={16} height={16} />}
+            onClick={action('download')}
+            size="default"
+          />
+        </Popover>
+      </Fragment>
+    </Fragment>
+  )
 }
 
 class ReportCardState extends React.PureComponent {
@@ -36,20 +116,21 @@ class ReportCardState extends React.PureComponent {
 
   render () {
     return (
-      <ReportCard
-        cardExpanded={this.state.expandedCard}
-        onClick={this.handleClick()}
-        report={report}
-        title="Relatório financeiro"
-        subTitle={
-          <div>
-            Período: {moment(report.data.created_at).format('DD/MM/YYYY')}
-            até {moment(report.data.updated_at).format('DD/MM/YYYY')} |
-            Criado: {moment(report.created_at).format('DD/MM/YYYY')}
-+         </div>
-        }
-      />
-
+     <Card>
+       <ReportCard
+         actions={renderActions(report)}
+         cardExpanded={this.state.expandedCard}
+         report={report}
+         title="Relatório financeiro"
+         subTitle={
+           <div>
+             Período: {moment(report.data.created_at).format('DD/MM/YYYY')}
+             até {moment(report.data.updated_at).format('DD/MM/YYYY')} |
+             Criado: {moment(report.created_at).format('DD/MM/YYYY')}
++          </div>
+         }
+       />
+     </Card>
     )
   }
 }
